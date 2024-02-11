7800f6af9fac588f51cada2ff01e0236d6ab1aea
Juliana kuedjeu <julianakeudjeu@gmail.com>
2024-02-11 20:17:09 +0100

    First migration done

--- ATG/apps.py
+++ ATG/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloConfig(AppConfig):
+class AtgConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hello'
+    name = 'ATG'
